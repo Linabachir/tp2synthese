d1c57a76666472c8cde2dd4e8f20f53b8087ff59
Linabachir <161326335+Linabachir@users.noreply.github.com>
2025-01-11 01:14:27 +0100

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -28,16 +28,16 @@ int main(int argc, char ** argv) {
 
     char ip_string[128]={0};
     char port_string[128]={0};
-
+   // Print the resolved server IP and port
     getnameinfo(result->ai_addr,result->ai_addrlen,
                 ip_string,128,port_string,128,
                 NI_NUMERICHOST|NI_NUMERICSERV);
 
     printf("%s is resolved at : %s:%s\n",argv[1],ip_string,port_string);
-
+// Create a UDP socket
     int sd = socket(result->ai_family,result->ai_socktype,
                     result->ai_protocol);
-
+   // Build and send the RRQ (Read Request)
     char rrq[128]={0};
     rrq[1]=1;
     sprintf(rrq+2,"%s",argv[3]);
@@ -48,6 +48,7 @@ int main(int argc, char ** argv) {
 
     int bytes;
     do{
+        // Receive data packets from the server
         char buffer[516]={0};
 
         struct sockaddr data_connexion;
@@ -58,9 +59,9 @@ int main(int argc, char ** argv) {
             printf("Error during transmission on block %d : %s\n",buffer[3],buffer+4);
             exit(EXIT_FAILURE);
         }
-
+// Write received data to the file
         write(fd,buffer+4,bytes-4);
-
+  // Send acknowledgment (ACK) to the server
         char ack[4]={0};
         ack[1]=4;
         ack[2]=buffer[2];
